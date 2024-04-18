ab51eb5659e6d6904cb7ba5007ff3be8d3d63ac0
shwetaanair <149757672+shwetaanair@users.noreply.github.com>
2024-04-18 10:08:03 +0000

    Create package1Arithmetic.java

---
+++ package1Arithmetic.java
@@ -0,0 +1,29 @@
+package arithmetic_package;
+
+interface interface_graphics{
+   public float add(int a, int b);
+   public float divide(int a, int b);
+   public float multiple(int a, int b);
+   public float substract(int a, int b);
+   public float remainder(int a, int b);
+}
+
+public class Arithmetic implements interface_graphics {
+    
+    public float add(int a, int b){
+        return a+b;
+    }
+    public float divide(int a, int b){
+        return a/b;
+    }
+    public float multiple(int a, int b){
+        return a*b;
+    }
+    public float substract(int a, int b){
+        return a-b;
+    }
+    public float remainder(int a, int b){
+        return a%b;
+    }
+    
+}
